68161f97382877b82a38e8682cdbc76269ebf883
Colton Ogden <coltonoscopy@gmail.com>
2022-09-17 22:32:04 -0700

    slight code change to see if triggers rebuild

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Chucky!!!
+          Testing changing stuff
         </a>
       </header>
     </div>
